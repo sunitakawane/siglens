3fb3f0a5f655b0d68c056eb454d2b59ea78efed7
sunitakawane <skawane@sigscalr.io>
2023-12-19 14:26:04 +0530

    test changes for prod build

--- package_siglens.sh
+++ package_siglens.sh
@@ -45,8 +45,10 @@ for platform in "${platforms[@]}"; do
     if [ ${GOOS} = "darwin" ]; then
         if [ ${GOARCH} = "arm64" ]; then
             export CC="clang -arch ${GOARCH}"
+            export CGO_CFLAGS="-arch ${GOARCH}"
+            export CGO_LDFLAGS="-arch ${GOARCH}"
             export CFLAGS=-Qunused-arguments
-	        export CPPFLAGS=-Qunused-arguments
+            export CPPFLAGS=-Qunused-arguments
             export CGO_ENABLED=1
             export GOGCCFLAGS="-m64 -mthreads -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -gno-record-gcc-switches"
             echo "Compiling SigLens for CGO_ENABLED=1, GOOS=${GOOS} and GOARCH=${GOARCH}"
@@ -54,8 +56,10 @@ for platform in "${platforms[@]}"; do
         fi
         if [ ${GOARCH} = "amd64" ]; then
             export CC="clang"
+            export CGO_CFLAGS="-arch x86_64"
+            export CGO_LDFLAGS="-arch x86_64"
             export CFLAGS=-Qunused-arguments
-	        export CPPFLAGS=-Qunused-arguments
+            export CPPFLAGS=-Qunused-arguments
             export CGO_ENABLED=1
             export GOGCCFLAGS="-m64 -mthreads -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -gno-record-gcc-switches"
             echo "Compiling SigLens for CGO_ENABLED=1, GOOS=${GOOS} and GOARCH=${GOARCH}"
